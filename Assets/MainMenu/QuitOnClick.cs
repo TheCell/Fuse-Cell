61cb259d8609bc4a6f5094c234cc3432135fa362
TheCell <simon_hischier@hotmail.com>
2020-02-17 23:20:22 +0100

    Minimal Menu, added Input System

---
+++ Assets/MainMenu/QuitOnClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour
+{
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/MainMenu/StartScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    public void StartSceneByID(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
